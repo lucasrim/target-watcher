8322c77432fc74851179fee8f3d058e7cf905388
lucas <lucasrim@gmail.com>
2020-12-05 15:17:54 -0600

    Add Support for both disk and digital

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 import yargs from 'yargs';
+import Discord from 'discord.js';
 
 type AvailabilityStatus = {
   availability_status: 'OUT_OF_STOCK' | 'UNAVAILABLE' | 'IN_STOCK';
@@ -26,34 +27,52 @@ type Location = {
   in_store_only: AvailabilityStatus;
 };
 
+type BuyOption =
+  | 'order_pickup'
+  | 'curbside'
+  | 'ship_to_store'
+  | 'in_store_only';
+
+type ConsoleType = 'disk' | 'digital';
+
 const args = yargs.options({
   zipcode: { type: 'string', demandOption: true, alias: 'z' },
 }).argv;
 
-const requestString = (zipcode: string): string =>
-  `https://api.target.com/fulfillment_aggregator/v1/fiats/81114595?key=ff457966e64d5e877fdbad070f276d18ecec4a01&nearby=${zipcode}&limit=20&requested_quantity=1&radius=75&fulfillment_test_mode=grocery_opu_team_member_test`;
+const requestString = (
+  zipcode: string,
+  consoleType: 'disk' | 'digital',
+): string =>
+  `https://api.target.com/fulfillment_aggregator/v1/fiats/${
+    consoleType === 'disk' ? '81114595' : '81114596'
+  }?key=ff457966e64d5e877fdbad070f276d18ecec4a01&nearby=${zipcode}&limit=20&requested_quantity=1&radius=75`;
 
-const fetchTarget = (callback: any) =>
-  fetch(requestString(args['zipcode']), {
+const fetchTarget = (callback: any, consoleType: ConsoleType) =>
+  fetch(requestString(args['zipcode'], consoleType), {
     method: 'GET',
   })
     .then((res) => res.json())
-    .then((json) => callback(json));
+    .then((json) => {
+      console.log(
+        `Checking stores near ${args['zipcode']} for ${consoleType}:`,
+      );
+      callback(json);
+    });
+
+const findLocationsInStock = (
+  locations: Location[],
+  buyOption: BuyOption,
+): Location[] =>
+  locations.filter(
+    (location) => location[buyOption].availability_status === 'IN_STOCK',
+  );
 
 const locationsWatcher = (response: TargetResponse) => {
   const locations = response.products[0].locations;
-  const orderPickupAvailable = locations.filter(
-    (location) => location.order_pickup.availability_status === 'IN_STOCK',
-  );
-  const curbsideAvailable = locations.filter(
-    (location) => location.curbside.availability_status === 'IN_STOCK',
-  );
-  const shipToStoreAvailable = locations.filter(
-    (location) => location.ship_to_store.availability_status === 'IN_STOCK',
-  );
-  const inStoreAvailable = locations.filter(
-    (location) => location.in_store_only.availability_status === 'IN_STOCK',
-  );
+  const orderPickupAvailable = findLocationsInStock(locations, 'order_pickup');
+  const curbsideAvailable = findLocationsInStock(locations, 'curbside');
+  const shipToStoreAvailable = findLocationsInStock(locations, 'ship_to_store');
+  const inStoreAvailable = findLocationsInStock(locations, 'in_store_only');
 
   console.log(`Order Pickup Available: ${orderPickupAvailable}`);
   console.log(`Curbside Available: ${curbsideAvailable}`);
@@ -61,14 +80,16 @@ const locationsWatcher = (response: TargetResponse) => {
   console.log(`In Store Available: ${inStoreAvailable}`);
 };
 
-async function asyncTimeout(delay: number): Promise<any> {
+async function asyncTimeout(
+  delay: number,
+  consoleType: ConsoleType,
+): Promise<any> {
   return new Promise(async (resolve) => {
-    console.log(`Checking stores near ${args['zipcode']}:`);
-    console.log();
-    await fetchTarget(locationsWatcher);
+    await fetchTarget(locationsWatcher, consoleType);
     console.log();
     setTimeout(() => resolve(delay), delay);
-  }).then((x) => asyncTimeout(x as number));
+  }).then((x) => asyncTimeout(x as number, consoleType));
 }
 
-asyncTimeout(10000);
+asyncTimeout(10000, 'disk');
+asyncTimeout(10000, 'digital');
